1a0df8b627260c070b1300a08addabed0e796745
Zac Hayes <zrev2220@gmail.com>
2022-06-04 15:03:02 -0400

    Call cleanup manually

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,8 +1,13 @@
 import "@testing-library/jest-dom";
 
+import { cleanup } from "@testing-library/react";
+
 declare global {
   var IS_REACT_ACT_ENVIRONMENT: boolean;
 }
 
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
+afterEach(() => {
+  cleanup();
+});
